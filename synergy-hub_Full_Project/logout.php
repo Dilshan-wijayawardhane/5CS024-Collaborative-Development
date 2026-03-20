0a2049d88539f0c9cbbc1b37066ef95faae218b2
Dilshan-wijayawardhane <dilshanwijawardhana@gmail.com>
2026-03-20 14:26:34 +0530

    Update logout.php

--- synergy-hub_Full_Project/logout.php
+++ synergy-hub_Full_Project/logout.php
@@ -4,7 +4,7 @@ require_once 'functions.php';
 
 // Log the logout activity if user was logged in
 if (isset($_SESSION['user_id'])) {
-    // We need to include config.php here because functions.php doesn't have it
+    
     require_once 'config.php';
     logActivity($conn, $_SESSION['user_id'], 'LOGOUT');
 }
