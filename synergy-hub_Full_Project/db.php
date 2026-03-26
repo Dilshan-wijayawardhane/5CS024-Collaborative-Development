187d24d3c4e80098a4d41d1328227247cf058fca
Manthusi Karunasinghe <manthusik@gmail.com>
2026-03-26 23:37:33 +0530

    Update db.php

--- synergy-hub_Full_Project/db.php
+++ synergy-hub_Full_Project/db.php
@@ -1,17 +1,20 @@
 <?php
-/**
- * Central database connection file for Synergy Hub project.
- */
+
+
 
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "collaborative_dev";
 
-// Establish connection to MySQL database
+
+
+
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check connection
+
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
