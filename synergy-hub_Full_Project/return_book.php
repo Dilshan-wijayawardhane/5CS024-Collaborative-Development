4eba5e608dc4ca0c44c9e70e7937ccf778e2b2a4
Prasadi Malsha Abeysekara <prasadimalshapma@gmail.com>
2026-03-27 20:53:34 +0530

    Update return_book.php

--- synergy-hub_Full_Project/return_book.php
+++ synergy-hub_Full_Project/return_book.php
@@ -14,27 +14,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['borrow_id']) && isset(
     $borrow_id = intval($_POST['borrow_id']);
     $book_id = intval($_POST['book_id']);
     
-    // Update borrow record
+   
     $return_sql = "UPDATE borrowed_books SET return_date = CURDATE(), status = 'returned' 
                     WHERE borrow_id = ? AND user_id = ? AND status = 'borrowed'";
     $return_stmt = mysqli_prepare($conn, $return_sql);
     mysqli_stmt_bind_param($return_stmt, "ii", $borrow_id, $user_id);
     
     if (mysqli_stmt_execute($return_stmt) && mysqli_stmt_affected_rows($return_stmt) > 0) {
-        // Update book availability
+        
         $update_sql = "UPDATE books SET available = available + 1 WHERE book_id = ?";
         $update_stmt = mysqli_prepare($conn, $update_sql);
         mysqli_stmt_bind_param($update_stmt, "i", $book_id);
         mysqli_stmt_execute($update_stmt);
         
-        // Award points for returning (2 points)
+        
         $points = 2;
         $points_sql = "UPDATE Users SET PointsBalance = PointsBalance + ? WHERE UserID = ?";
         $points_stmt = mysqli_prepare($conn, $points_sql);
         mysqli_stmt_bind_param($points_stmt, "ii", $points, $user_id);
         mysqli_stmt_execute($points_stmt);
         
-        // Log activity
+       
         logActivity($conn, $user_id, 'RETURN_BOOK', 'books', $book_id);
         
         echo json_encode(['success' => true, 'message' => 'Book returned']);
